397dc0a4a292be3ee55496c25a3199f6e04b7a90
Renan <renan.scorp@gmail.com>
2021-04-24 20:51:19 -0300

    First commit

---
+++ src/pages/types/Episode.tsx
@@ -0,0 +1,11 @@
+export type Episode = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    members: string;
+    description: string;
+    duration: number;
+    durationAsString: string;
+    url: string;
+    publishedAt: string;
+}
